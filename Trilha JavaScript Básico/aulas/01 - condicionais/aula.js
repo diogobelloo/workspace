e4d96c84b0387f9c09184fe4319158e8c559ab81
Perkles <diogobelo69@gmail.com>
2022-11-09 17:16:43 -0300

    commit curso

---
+++ Trilha JavaScript Básico/aulas/01 - condicionais/aula.js
@@ -0,0 +1,9 @@
+const numero = 15;
+
+const numeroPar = numero % 3 === 0;
+
+if(numeroPar){
+    
+}
+
+console.log(numeroPar)
---
+++ Trilha JavaScript Básico/aulas/02 - Variaveis e operadores/aula.js
@@ -0,0 +1,18 @@
+//Algoritimo para saber quantos de gasolina irei gastar por uma determinada viajem
+
+//faça um programa para calcular o valor gasto de combustível em uma viagem.
+
+
+
+let preco = 5.60
+
+let mediaKm = 12
+
+let distanciaKm = 144 
+
+let gastoKm = preco / mediaKm 
+
+let totalGasto = gastoKm * distanciaKm
+
+console.log(totalGasto)
+
