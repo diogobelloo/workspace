94aeb1608e4aa341301d72e7a4200bd5e4421f0e
Perkles <diogobelo69@gmail.com>
2022-11-14 18:54:32 -0300

    comitando aula funcoes

---
+++ Trilha JavaScript Básico/aulas/04 - Funcoes/ex.js
@@ -0,0 +1,14 @@
+function soma(n1 , n2){
+   return n1 + n2
+}
+
+
+
+
+var n1 = 10
+var n2 = 20
+
+const somaa =soma( n1 , n2 )
+
+console.log(somaa)
+
---
+++ Trilha JavaScript Básico/aulas/04 - Funcoes/imc-funcoes.js
@@ -0,0 +1,35 @@
+function calcularImc(peso, altura){
+    return  peso / (altura * altura)
+}
+
+function classificarImc(imc){
+    if(imc < 18.5){
+        return 'abaixo do peso'
+    }
+    else if(imc >= 18.5 && imc < 25){
+        return 'peso normal'
+    }
+    else if(imc >= 25 && imc < 30){
+       return 'acima do peso'
+    }
+    else if(imc >= 30 && imc <=   40){
+        return 'acima do peso'
+    }
+    else{
+        return 'obesidade grave'
+    }
+}
+
+function main(){
+
+    const peso = 64
+    const altura = 1.75
+
+    const imc = peso / calcularImc(peso, altura) //altura ao quadrado
+    console.log(classificarImc(imc))
+}
+
+main();
+
+
+
