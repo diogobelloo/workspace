be8348218dc33579842bf8296aa66aa1d368163c
Perkles <diogobelo69@gmail.com>
2022-11-22 22:59:15 -0300

    aprendendo classes

--- Trilha JavaScript Básico/aulas/05 - Objetos-e-Classes/exercicios/exercicio02.js
+++ Trilha JavaScript Básico/aulas/05 - Objetos-e-Classes/exercicios/exercicio02.js
@@ -1,27 +1,4 @@
-/* 
-2 - crie uam classe para representar pessoas.
-Para cada pessoa teremos os atributos nome, peso e altura.
-As pessoas devem ter a capacidade de dizer o valor do seu IMC (IMC= peso / (altura * altura));
-Instancie uma pessoa chamada José que tenha 70kg de peso e 1,75 de altura e peça ao José para dizer o valor do seu IMC
-
-*/
-
-
-class Pessoa{
-    nome;
-    peso;
-    altura;
-
-    constructor(nome, peso, altura){
-        this.nome = nome;
-        this.peso = peso;
-        this.altura = altura;
-    }
-
-    calcularImc(){
-        return this.peso / (this.altura * this.altura)
-    }
+const dev = {
+    nome: 'diogo'
 }
-
-const Diogo = new Pessoa('Diogo', 63, 1.70)
-console.log(Diogo.calcularImc()) 
+console.log(dev.nome)
