960ec7126c6cb95a51a63551ca7d12c2a2411b34
Perkles <diogobelo69@gmail.com>
2022-11-27 19:25:14 -0300

    aprendendo sobre loops

--- Trilha JavaScript Básico/aulas/06 - Arrays-e-estruturas-de-repetição/aula04.js
+++ Trilha JavaScript Básico/aulas/06 - Arrays-e-estruturas-de-repetição/aula04.js
@@ -6,10 +6,17 @@ const numeros = [1,2,3,4,5,6,7,8,9,10];
 
 for(i=0; i <= numeros.length; i++){
    const numero = numeros[i]
+   if(numero % 2 === 0){
+    console.log(numero)
+   }
+  
+    }
+  
 
-  }
 
-
+const listadevalor = [1, 2, 3, 4]
+const valor = listadevalor[2]
+console.log(valor)
 
 
     
