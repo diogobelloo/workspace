6d3992fcb049b87aec3af56e97232168514109dd
Perkles <diogobelo69@gmail.com>
2022-11-27 16:46:00 -0300

    aprendendo sobre loops

--- Trilha JavaScript Básico/aulas/06 - Arrays-e-estruturas-de-repetição/aula03.js
+++ Trilha JavaScript Básico/aulas/06 - Arrays-e-estruturas-de-repetição/aula03.js
@@ -1,24 +1,7 @@
+let numero = 5;
 
-const nome = 'Diogo Belo Dos Santos'
-
-const notas = [];
-
-notas.push(4);
-notas.push(3);
-notas.push(8);
-notas.push(10);
-notas.push(10);
-notas.push(4);
-notas.push(4);
-
-let soma = 0;
-
-for (let i = 0; i < notas.length; i++) {
-    const nota = notas[i];
-    soma = soma + nota;
-    
-}
+for(indice=1; indice<=1000; indice++){
+    console.log(`${numero} + ${indice} = ${numero * indice}`);
 
 
-const media = soma / notas.length;  // notas.length são a quantidade de arrays que tenho no push
-console.log(media);
+  }
---
+++ Trilha JavaScript Básico/aulas/06 - Arrays-e-estruturas-de-repetição/aula04.js
@@ -0,0 +1,16 @@
+const numeros = [1,2,3,4,5,6,7,8,9,10];
+
+
+
+
+
+for(i=0; i <= numeros.length; i++){
+   const numero = numeros[i]
+
+  }
+
+
+
+
+    
+  
