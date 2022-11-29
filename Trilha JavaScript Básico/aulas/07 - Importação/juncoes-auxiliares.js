5354b9eb4aa2cde69f793ff07812ccf447171a40
Perkles <diogobelo69@gmail.com>
2022-11-29 18:23:45 -0300

    aprendendo sobre loops

--- Trilha JavaScript Básico/aulas/07 - Importação/juncoes-auxiliares.js
+++ Trilha JavaScript Básico/aulas/07 - Importação/juncoes-auxiliares.js
@@ -1,9 +1,16 @@
+const consjuntosDeEntradas = [ 5, 50, 10, 98, 23];
+let i = 0;
+
+
+
 function gets(){
-    return 10;
+   const valor = consjuntosDeEntradas[i];
+   i = i + 1 //ou  i++
+   return valor;
 }
 
 function print(texto){
-    console,log(texto)
+    console.log(texto)
 }
 
 
--- Trilha JavaScript Básico/aulas/07 - Importação/main.js
+++ Trilha JavaScript Básico/aulas/07 - Importação/main.js
@@ -1,7 +1,42 @@
 const {gets, print} = require('./juncoes-auxiliares')   //importando minhas funções gets e print
 
-const pessoa = {
-    nome:'vitor'
-}
+print(gets());
+print(gets());
+print(gets());
+print(gets());
+print(gets());
+
+
+/* Uma sala contem 5 alunos e para cada aluno foi sorteado um numero de 1 - 100.
+Faça um programa e esreceva os 4 numeros sorteados para os alunos e mostre o maior numero sorteado.
+
+Dados de entrada:
+5
+50
+10
+98
+23
+
+
+Saída:
+98
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 print(gets());
