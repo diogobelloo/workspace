25a9606a9850b42763c9404f7808d97067633531
Perkles <diogobelo69@gmail.com>
2022-11-30 18:22:19 -0300

    commit curso JS

---
+++ Trilha JavaScript Básico/aulas/07 - Importação/exercicio2.js
@@ -0,0 +1,12 @@
+const { gets, print } = require('./juncoes-auxiliares')
+
+let numero1 = 5
+let numero2 = 20
+
+for(let i = 1; i < 1000; i++) {
+    if(i % numero1){
+        const soma = 0;
+        soma+=i
+    }
+    
+}
---
+++ Trilha JavaScript Básico/aulas/Function/index.js
@@ -0,0 +1,50 @@
+
+const livro = {
+    nome :"React Native",
+    editor: "Casa do código",
+    paginas: 185, 
+    anunciar : function(){
+        console.log("A Alura indica o livro" + this.nome+ "!")
+    }
+    
+}
+
+livro.anunciar()
+
+
+///////////////////////////
+
+class Livro {
+    
+    constructor(nome, editora, paginas){
+        this.nome = nome;
+        this.editora = editora;
+        this.paginas = paginas;
+
+    }
+    anunciarTitulo(){
+        console.log(`Título: ${this.nome}`)
+    }
+    descreverTitulo(){
+        console.log(`${this.nome} é um livro da editora ${this.editora} e tem ${this.paginas} páginas.`)
+    }
+}
+
+const nodeJs = new Livro('Diogo', 'Leda', 200);
+
+nodeJs.anunciarTitulo()
+nodeJs.descreverTitulo()
+
+
+class LivroColecao extends Livro {
+    constructor(nome, nomeColecao){
+        super(nome)       //Só estou pegando o this.nome da minha outra class
+        this.nomeColecao = nomeColecao
+    }
+    descreverColecao(){
+        console.log(`O livro ${this.nome}, faz parte da coleção ${this.nomeColecao}.`)
+    }
+}
+
+var LogicaDeProgramacao = new LivroColecao('Diogo', 'Nova era')
+LogicaDeProgramacao.descreverColecao()
