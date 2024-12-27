2e3e510766dc3f7795327347c14d3dd550fad43b
Diogo belo <diogobelo69@gmail.com>
2024-12-27 15:19:39 -0300

    push nos arquivos de exercicios loop JS

---
+++ Trilha JavaScript/aulas/08 - loops/exercicio02/exercicioloops.js
@@ -0,0 +1,20 @@
+function soma() {
+let num = document.getElementById("num");
+let list = document.getElementById("list")
+let n = Number(num.value);
+
+list.innerHTML = ''
+
+if(num.value.length == 0 ){
+    window.alert('[ERRO] Digite um número');
+}
+else {
+    for(let i = 0; i <= 10; i++){
+       let item = document.createElement("option");
+       item.innerText = `${n} * ${i} = ${n * i}`
+       list.appendChild(item);
+    }
+}
+
+
+}
