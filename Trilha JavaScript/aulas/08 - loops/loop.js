a52e7a90ea0abaffc519a62bc16e6ce8a3ab7d80
Diogo belo <diogobelo69@gmail.com>
2024-12-27 00:27:29 -0300

    Atualização modulo de loops no curso JS

---
+++ Trilha JavaScript/aulas/08 - loops/loop.js
@@ -0,0 +1,16 @@
+let e = 1
+while (e <= 30) {
+    i++
+    console.log(i)
+}
+
+
+let a = 1
+do {
+    a++
+    console.log(a)
+} while (a <= 10);
+
+for(let z = 1; z <=10; z++){
+    console.log
+}
