603f5f3cc27d5a956cc1723d08a456d93136e0fe
Diogo belo <diogobelo69@gmail.com>
2025-01-01 22:35:48 -0300

    Resolução exercicio JS arrays

--- Trilha JavaScript/aulas/11-Exercicio-revisão/analiseNuemros/main.js
+++ Trilha JavaScript/aulas/11-Exercicio-revisão/analiseNuemros/main.js
@@ -2,7 +2,8 @@
 const btnAdd = document.querySelector('#btnAdd');
 const select = document.querySelector('#select');
 const numberValue = document.querySelector('#valueInp');
-
+const result = document.querySelector('#resList');
+const btnList = document.querySelector('#btnList');
 
 
 const addArray = (numberValue) => {
@@ -14,11 +15,23 @@ const addArray = (numberValue) => {
 
 const criaOption = () => {
     //select.textContent ='' caso queira que seja adicinado e apagado.
+    if(numberValue.value == 0 || numberValue.value > 100){
+        window.alert('digite um número valido')
+        option.textContent = ' '
+    } 
     const option = document.createElement('option');
     option.textContent = `Valor adicionado foi : ${addArray(numberValue)}`
     select.appendChild(option)
 }
 
+const createResult = (arrayNumber) => {
+    const li = document.createElement("li");
+  
+    li.textContent = `Temos ao todo ${arrayNumber.length}`;
+    result.appendChild(li);
+}
+ 
 btnAdd.addEventListener('click', criaOption)
+btnList.addEventListener('click', createResult)
 
     
